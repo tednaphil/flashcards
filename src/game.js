f6289ed27ed08e20bbd4f15b93056ceb9f51a2d6
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-30 21:09:30 -0600

    Create cards

--- src/game.js
+++ src/game.js
@@ -1,6 +1,13 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const {createCard} = require('./card');
+
+let cards = prototypeQuestions.map((object) => {
+  return createCard(object.id, object.question, object.answers, object.correctAnswer);
+});
+
+// console.log(cards)
 
 function printMessage(deck) {
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
