1747d3fe21df8f48d24d45336f729205251cbc0e
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-30 21:20:15 -0600

    Add start function

--- src/game.js
+++ src/game.js
@@ -5,12 +5,6 @@ const {createCard} = require('./card');
 const {createDeck, countCards} = require('./deck');
 const {evaluateGuess, createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
 
-let cards = prototypeQuestions.map((object) => {
-  return createCard(object.id, object.question, object.answers, object.correctAnswer);
-});
-let deck = createDeck(cards);
-let round = createRound(deck);
-// console.log('round object: ', round)
 
 function printMessage(deck) {
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
@@ -19,6 +13,16 @@ function printMessage(deck) {
 
 function printQuestion(round) {
   util.main(round);
+}
+
+function start() {
+  let cards = prototypeQuestions.map((object) => {
+    return createCard(object.id, object.question, object.answers, object.correctAnswer);
+  });
+  let deck = createDeck(cards);
+  let round = createRound(deck);
+  printMessage(deck);
+  printQuestion(round);
 }
 
 module.exports = { printMessage, printQuestion };
