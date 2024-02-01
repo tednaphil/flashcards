46291c9fc7ad7935b51483ede754a4b3a1dedaba
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-01 17:21:06 -0600

    Import test data and update assertions

--- test/round-test.js
+++ test/round-test.js
@@ -1,19 +1,21 @@
 const chai = require('chai');
 const expect = chai.expect;
+const data = require('./data-test');
+const ques = data.testData;
 
 const {createCard} = require('../src/card');
-const {createDeck, countCards} = require('../src/deck');
+const {createDeck} = require('../src/deck');
 const {evaluateGuess, createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
 
 describe('Round', function() {
   let card1, card2, card3, card4, deck, round;
   beforeEach(() => {
-        card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-        card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-        card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-        card4 = createCard(3, 'What is Bellamy\'s favorite snack?', ['club crackers', 'babybel cheese', 'blueberries'], 'club crackers');
-        deck = createDeck([card1, card2, card3, card4]);
-        round = createRound(deck);
+    card1 = createCard(ques[0].id, ques[0].question, ques[0].answers, ques[0].correctAnswer);
+    card2 = createCard(ques[1].id, ques[1].question, ques[1].answers, ques[1].correctAnswer);
+    card3 = createCard(ques[2].id, ques[2].question, ques[2].answers, ques[2].correctAnswer);
+    card4 = createCard(ques[3].id, ques[3].question, ques[3].answers, ques[3].correctAnswer);
+    deck = createDeck([card1, card2, card3, card4]);
+    round = createRound(deck);
     });
 
     describe('Evaluate Guess', function(){
@@ -23,13 +25,13 @@ describe('Round', function() {
         });
     
         it('should give feedback if guess is correct', function() {
-            let guess = 'club crackers';
+            let guess = 'Maurice Sendak';
             
             expect(evaluateGuess(guess, card4)).to.equal('Correct!');
         });
     
         it('should give feedback if guess is incorrect', function() {
-            let guess = 'babybel cheese';
+            let guess = 'Shel Silverstein';
     
             expect(evaluateGuess(guess, card4)).to.equal('Incorrect!');
         });
@@ -47,9 +49,9 @@ describe('Round', function() {
             expect(round.deck).to.deep.equal([card1, card2, card3, card4]);
             expect(round.currentCard).to.deep.equal({
                      id: 1,
-                     question: 'What is Robbie\'s favorite animal',
-                     answers: ['sea otter', 'pug', 'capybara'],
-                     correctAnswer: 'sea otter'
+                     question: "Who wrote 'Charlie and the Chocolate Factory'?",
+                     answers: ["Roald Dahl", "J.K. Rowling", "Dr. Seuss"],
+                     correctAnswer: "Roald Dahl"
                    });
             expect(round.turns).to.equal(0);
             expect(round.incorrectGuesses).to.deep.equal([]);
@@ -58,19 +60,19 @@ describe('Round', function() {
     
     describe('Take Turn', function() {
         it('should return feedback if the guess for the round is correct', function() {
-            let turn = takeTurn('sea otter', round);
+            let turn = takeTurn('Roald Dahl', round);
 
             expect(turn).to.equal('Correct!');
         });
         
         it('should return feedback if the guess for the round is incorrect', function() {
-            let turn = takeTurn('pug', round);
+            let turn = takeTurn('Dr. Seuss', round);
 
             expect(turn).to.equal('Incorrect!');
         });
 
         it('should record incorrect guesses', function() {
-            let turn = takeTurn('capybara', round);
+            let turn = takeTurn('J.K. Rowling', round);
 
             expect(turn).to.equal('Incorrect!')
             expect(round.incorrectGuesses).to.deep.equal([1]);
@@ -83,13 +85,13 @@ describe('Round', function() {
         });
 
         it('should count turns', function() {
-            takeTurn('sea otter', round);
+            takeTurn('Roald Dahl', round);
 
             expect(round.turns).to.equal(1);
         });
 
         it('should only count the turn if a valid answer choice is guessed', function() {
-            takeTurn('sea otter', round);
+            takeTurn('Roald Dahl', round);
             takeTurn(null, round);
 
             expect(round.turns).to.equal(1);
@@ -97,47 +99,48 @@ describe('Round', function() {
         });
 
         it('should set the next card in the deck as the current card after a turn', function() {
-            let turn1 = takeTurn('sea otter', round);
-            let turn2 = takeTurn('spleen', round);
+            let turn1 = takeTurn('Roald Dahl', round);
+            let turn2 = takeTurn('Errol', round);
             let invalidTurn = takeTurn(null, round);
-            let turn3 = takeTurn('Lex', round);
+            let turn3 = takeTurn('Middle-earth', round);
 
             expect(round.currentCard).to.deep.equal(card4)
             expect(turn3).to.equal('Incorrect!');
             expect(round.turns).to.equal(3);
-            expect(round.incorrectGuesses).to.deep.equal([14, 12])
+            expect(round.incorrectGuesses).to.deep.equal([2, 3])
         });
     });
-    
+
     describe('Calculate Percent Correct', function() {
-        let turn1, turn2, invalidTurn, turn3, turn4;
-        beforeEach(() => {
-            turn1 = takeTurn('sea otter', round);
-            turn2 = takeTurn('spleen', round);
-            invalidTurn = takeTurn(null, round);
-            turn3 = takeTurn('Lex', round);
-            turn4 = takeTurn('club crackers', round);
+        // let turn1, turn2, invalidTurn, turn3, turn4;
+        // beforeEach(() => {
+        //     turn1 = takeTurn('Roald Dahl', round);
+        //     turn2 = takeTurn('Errol', round);
+        //     invalidTurn = takeTurn(null, round);
+        //     turn3 = takeTurn('Middle-earth', round);
+        //     turn4 = takeTurn('Maurice Sendak', round);
     
-        });
+        // });
 
         it('should return the percentage of correct guesses', function() {
+            takeTurn('Roald Dahl', round);
+            takeTurn('Errol', round);
+            takeTurn(null, round);
+            takeTurn('Middle-earth', round);
+            takeTurn('Maurice Sendak', round);
             
             expect((calculatePercentCorrect(round))).to.equal(50);
         });
     });
     
     describe('End Round', function() {
-        let turn1, turn2, invalidTurn, turn3, turn4;
-        beforeEach(() => {
-            turn1 = takeTurn('sea otter', round);
-            turn2 = takeTurn('spleen', round);
-            invalidTurn = takeTurn(null, round);
-            turn3 = takeTurn('Lex', round);
-            turn4 = takeTurn('club crackers', round);
-    
-        });
         
         it('should print the percentage of correct answers to the console', function() {
+            takeTurn('Roald Dahl', round);
+            takeTurn('Errol', round);
+            takeTurn(null, round);
+            takeTurn('Middle-earth', round);
+            takeTurn('Maurice Sendak', round);
 
             expect((endRound(round))).to.equal('**Round over!** You answered 50% of the questions correctly!');
         });
